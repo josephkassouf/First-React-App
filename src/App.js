1a5ce4da6d7ce09b24ae7f3ef203d96214f81895
Joseph Kassouf <jkassouf@upei.ca>
2022-11-10 22:40:16 -0400

    created a movie search library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MovieCard from "./components/MovieCard";
+import "./App.css";
+
+import SearchIcon from "./search.svg";
+
+const omdbUrl =
+  "http://www.omdbapi.com/?apikey=" + process.env.REACT_APP_API_KEY;
 
 function App() {
-  return <div>App</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${omdbUrl}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
